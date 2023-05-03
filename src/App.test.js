8367af269db4d87b7c842e022635472bc5af7c38
bsheairs <l33tguy42@gmail.com>
2023-05-03 12:38:59 -0400

    update unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,15 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
-test("renders input field and Get Signal button", () => {
+test("displays the trading signal after clicking Get Signal button", async () => {
   render(<App />);
   const inputElement = screen.getByPlaceholderText(/Enter stock ticker/i);
   const buttonElement = screen.getByText(/Get Signal/i);
 
-  expect(inputElement).toBeInTheDocument();
-  expect(buttonElement).toBeInTheDocument();
-});
-
-test("displays the trading signal after clicking Get Signal button", async () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/Get Signal/i);
-
-  // Mock the API response
-  jest.spyOn(global, "fetch").mockResolvedValue({
-    json: () =>
-      Promise.resolve({
-        "Time Series (Daily)": {
-          "2023-05-03": { "4. close": "100.00" },
-          "2023-05-02": { "4. close": "101.00" },
-          "2023-05-01": { "4. close": "102.00" },
-        },
-      }),
-  });
-
+  fireEvent.change(inputElement, { target: { value: "SPY" } });
   fireEvent.click(buttonElement);
 
-  await waitFor(() => screen.getByText(/The trading signal for/i));
+  await waitFor(() => screen.getByText(/Signal:/i));
 
-  expect(screen.getByText(/The trading signal for/i)).toBeInTheDocument();
-
-  // Clean up after the test
-  global.fetch.mockRestore();
+  expect(screen.getByText(/Signal:/i)).toBeInTheDocument();
 });
