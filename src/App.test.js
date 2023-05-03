c8c93ecc064d5db4248fc8ffa45c6ee503b34000
bsheairs <l33tguy42@gmail.com>
2023-05-03 13:27:44 -0400

    updated unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,21 @@
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
+
+test("renders the header", () => {
+  render(<App />);
+  const headerElement = screen.getByText(/AI Stock Signal/i);
+  expect(headerElement).toBeInTheDocument();
+});
 
 test("displays the trading signal after clicking Get Signal button", async () => {
   render(<App />);
-  const inputElement = screen.getByPlaceholderText(/Enter stock ticker/i);
+  const inputElement = screen.getByPlaceholderText("Enter stock ticker");
   const buttonElement = screen.getByText(/Get Signal/i);
 
-  fireEvent.change(inputElement, { target: { value: "SPY" } });
+  fireEvent.change(inputElement, { target: { value: "AAPL" } });
   fireEvent.click(buttonElement);
 
-  await waitFor(() => screen.getByText(/Signal:/i));
+  await waitFor(() => screen.getByText(/The trading signal for/i));
 
-  expect(screen.getByText(/Signal:/i)).toBeInTheDocument();
+  expect(screen.getByText(/The trading signal for/i)).toBeInTheDocument();
 });
