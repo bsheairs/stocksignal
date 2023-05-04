140470b8636b19647067415333bb97763cb451fb
bsheairs <l33tguy42@gmail.com>
2023-05-04 17:19:51 -0400

    Update calculations.js

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -2,24 +2,21 @@ export const calculateSMA = (prices, period) => {
   const result = new Array(prices.length).fill(null);
   let sum = 0;
 
-  // Reverse the input price data
-  const reversedPrices = [...prices].reverse();
+  for (let i = prices.length - 1; i >= 0; i--) {
+    sum += prices[i];
 
-  for (let i = 0; i < reversedPrices.length; i++) {
-    sum += reversedPrices[i];
-
-    if (i >= period) {
-      sum -= reversedPrices[i - period];
+    if (i <= prices.length - period) {
+      sum -= prices[i + period];
     }
 
-    if (i >= period - 1) {
+    if (i <= prices.length - period - 1) {
       result[i] = sum / period;
     }
   }
 
-  // Reverse the result back to the original order
-  return result.reverse();
+  return result;
 };
+
 
 export const calculateLastSignalDate = (sma50, sma200, dates) => {
   for (let i = dates.length - 1; i > 0; i--) {
